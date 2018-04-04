7eecc241e4341df81e1a4b01eb11accf43bced0d
alexfmanihuruk <alexfmanihuruk@gmail.com>
2018-04-04 16:56:59 +0700

    add project

---
+++ tugas1/src/main/java/com/tugas1/Tugas1_1706106614Application.java
@@ -0,0 +1,12 @@
+package com.tugas1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas1_1706106614Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas1_1706106614Application.class, args);
+	}
+}
