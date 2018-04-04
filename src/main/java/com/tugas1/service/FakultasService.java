a0aeb8569b7cd72e95648e3355d34c2e22e17d9c
alexfmanihuruk <alexfmanihuruk@gmail.com>
2018-04-04 16:51:13 +0700

    add project

---
+++ src/main/java/com/tugas1/service/FakultasService.java
@@ -0,0 +1,7 @@
+package com.tugas1.service;
+
+import com.tugas1.model.FakultasModel;
+
+public interface FakultasService extends DataService<FakultasModel> {
+
+}
---
+++ src/main/java/com/tugas1/service/ProgramStudiService.java
@@ -0,0 +1,7 @@
+package com.tugas1.service;
+
+import com.tugas1.model.ProgramStudiModel;
+
+public interface ProgramStudiService extends DataService<ProgramStudiModel> {
+
+}
