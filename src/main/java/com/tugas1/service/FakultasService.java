74f395716c5e07d516c03e7f895f412583b4f139
alexfmanihuruk <alexfmanihuruk@gmail.com>
2018-04-04 16:55:45 +0700

    modif

--- src/main/java/com/tugas1/service/FakultasService.java
+++
@@ -1,7 +0,0 @@
-package com.tugas1.service;
-
-import com.tugas1.model.FakultasModel;
-
-public interface FakultasService extends DataService<FakultasModel> {
-
-}
--- src/main/java/com/tugas1/service/UniversitasService.java
+++
@@ -1,7 +0,0 @@
-package com.tugas1.service;
-
-import com.tugas1.model.UniversitasModel;
-
-public interface UniversitasService extends DataService<UniversitasModel> {
-	UniversitasModel selectUniversitasByName(String name);
-}
